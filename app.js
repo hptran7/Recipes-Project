868337666dc13db02f69d9bbacb30f39a52c17a7
Tran Hung <tran.hung0713@gmail.com>
2020-10-30 15:50:53 -0500

    finish fetching API

--- app.js
+++ app.js
@@ -11,10 +11,29 @@ app.set('view engine','mustache')
 
 
 app.get('/',async(req,res)=>{
-    const resultData = await axios.get('https://api.edamam.com/search?q=chicken&app_id=28d11a29&app_key=0627b988aaf42dfa9db2bda0bf623c57&from=0&to=6&calories=591-722&health=alcohol-free')
-    console.log(resultData.data.hits)
+    const resultData = await axios.get('https://api.edamam.com/search?q=salad&app_id=28d11a29&app_key=0627b988aaf42dfa9db2bda0bf623c57&from=0&to=6&calories=591-722&health=alcohol-free')
+    console.log(resultData.data.hits[0].recipe.ingredientLines.join())
     res.render('index',{recipes:resultData.data.hits})
 })
+
+app.post('/add-recipe',(req,res)=>{
+    let image = req.body.image
+    let label = req.body.label
+    let ingredients = req.body.ingredients
+    let cookTime = req.body.cookTime
+    console.log(image)
+    console.log(label)
+    console.log(ingredients)
+    console.log(cookTime)
+    res.redirect('/')
+})
+
+app.post('/find-recipe',(req,res)=>{
+    const recipeSearch = req.body.recipeSearch
+    console.log(recipeSearch)
+    res.redirect('/')
+})
+
 
 
 app.listen(3000,()=>{
