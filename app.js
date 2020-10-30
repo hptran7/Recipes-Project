02627e882679cc074c7ae50f7ce6c916a6511bb3
Tran Hung <tran.hung0713@gmail.com>
2020-10-30 11:10:45 -0500

    fixing app.js

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ app.set('view engine','mustache')
 
 
 app.get('/',async(req,res)=>{
-    const resultData = await axios.get('')
-    res.render('index')
+    const resultData = await axios.get('https://api.edamam.com/search?q=chicken&app_id=28d11a29&app_key=0627b988aaf42dfa9db2bda0bf623c57&from=0&to=6&calories=591-722&health=alcohol-free')
+    console.log(resultData.data.hits)
+    res.render('index',{recipes:resultData.data.hits})
 })
 
 
