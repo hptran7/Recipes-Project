461519610fce845f0e62cbf01a543433d78d20cd
Tran Hung <tran.hung0713@gmail.com>
2020-10-30 09:07:48 -0500

    Project first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app= express()
+const mustacheExpress = require('mustache-express')
+
+app.use(express.urlencoded())
+
+app.engine('mustache',mustacheExpress())
+app.set('views','./views')
+app.set('view engine','mustache')
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server Is Running')
+})
